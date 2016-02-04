10576d36e016ef3c03d62c2c601d02c82ac1387a
dirgepye <s.stenhouse87@gmail.com>
2016-02-04 18:06:04 +0000

    Logins v2

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-(function ($) {
-  $(document).ready(function(){
-    
-	// hide .navbar first
-	$(".navbar").hide();
+$(document).ready(function() {
+	$('.style').click(function() {
+		console.log('test');
+		//var e = document.getElementsByClassName('form-basic');
+		
+		$('.form-basic').toggleClass('style-class');
+		//$('.form-basic').removeClass('style-class-two')
+		
+	});
+	$('.ringading').click(function() {
+		console.log('test');
+		//var e = document.getElementsByClassName('form-basic');
+		$('.form-basic').toggleClass('style-class-two');
+	});
+	$('.doodledoo').click(function() {
+		console.log('test');
+		//var e = document.getElementsByClassName('form-basic');
+		$('.form-basic').toggleClass('style-class-three');
+	});
 	
-	// fade in .navbar
-	$(function () {
-		$(window).scroll(function () {
-            // set distance user needs to scroll before we fadeIn navbar
+	
+	$('.sign-up').click(function() {
+		console.log('test');
+		$('.fields').append('<input type="text" id="login-username" placeholder="username" class="form-two__field"><input type="text" id="login-password" placeholder="password" class="form-two__field"><input type="text" id="login-username" placeholder="confirm password" class="form-two__field">');
+	});
+	
+	
+	
+	
+	$(".navbar").hide();
+	$(function() {
+		$(window).scroll(function() {
 			if ($(this).scrollTop() > 100) {
 				$('.navbar').fadeIn();
-			} else {
+			}
+			else {
 				$('.navbar').fadeOut();
 			}
 		});
-
-	
 	});
-
+	
 });
-  }(jQuery));
