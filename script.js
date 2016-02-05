1a33795d09515e23ca342e2380316ef538275bf1
dirgepye <s.stenhouse87@gmail.com>
2016-02-05 04:59:57 +0000

    added second style to first form

--- script.js
+++ script.js
@@ -1,22 +1,71 @@
 $(document).ready(function() {
-	$('.style').click(function() {
-		console.log('test');
-		//var e = document.getElementsByClassName('form-basic');
+	// $('.style').click(function() {
+	// 	console.log('test');
+	// 	//var e = document.getElementsByClassName('form-basic');
 		
-		$('.form-basic').toggleClass('style-class');
-		//$('.form-basic').removeClass('style-class-two')
+	// 	$('.form-basic').toggleClass('style-class');
+	// 	//$('.form-basic').removeClass('style-class-two')
 		
+	// });
+	// $('.ringading').click(function() {
+	// 	console.log('test');
+	// 	//var e = document.getElementsByClassName('form-basic');
+	// 	$('.form-basic').toggleClass('style-class-two');
+	// });
+	// $('.doodledoo').click(function() {
+	// 	console.log('test');
+	// 	//var e = document.getElementsByClassName('form-basic');
+	// 	$('.form-basic').toggleClass('style-class-three');
+	// });
+	
+	
+	$('.style-one').on('click', function() {
+		if ($('.form-one__basic').hasClass('style-class-two style-class-three')) {
+			$('.form-one__basic').removeClass('style-class-two style-class-three');
+			$('.form-one__basic').addClass('style-class-one');
+		}
+		
+		else if ($('.form-one__basic').hasClass('style-class-one')) {
+			$('.form-one__basic').removeClass('style-class-one');
+		}
+		else {
+			$('.form-one__basic').addClass('style-class-one');
+		}
 	});
-	$('.ringading').click(function() {
-		console.log('test');
-		//var e = document.getElementsByClassName('form-basic');
-		$('.form-basic').toggleClass('style-class-two');
+	
+	$('.style-two').on('click', function() {
+		if ($('.form-one__basic').hasClass('style-class-one style-class-three')) {
+			$('.form-one__basic').removeClass('style-class-one style-class-three');
+			$('.form-one__basic').addClass('style-class-two');
+		}
+		
+		else if ($('.form-one__basic').hasClass('style-class-two')) {
+			$('.form-one__basic').removeClass('style-class-two');
+		}
+		else {
+			$('.form-one__basic').addClass('style-class-two');
+		}
 	});
-	$('.doodledoo').click(function() {
-		console.log('test');
-		//var e = document.getElementsByClassName('form-basic');
-		$('.form-basic').toggleClass('style-class-three');
+	
+	$('.style-three').on('click', function() {
+		if ($('.form-one__basic').hasClass('style-class-one style-class-two')) {
+			$('.form-one__basic').removeClass('style-class-one style-class-two');
+			$('.form-one__basic').addClass('style-class-three');
+		}
+		
+		else if ($('.form-one__basic').hasClass('style-class-three')) {
+			$('.form-one__basic').removeClass('style-class-three');
+		}
+		else {
+			$('.form-one__basic').addClass('style-class-three');
+		}
 	});
+	
+	
+	
+	
+	
+	
 	
 	
 	$('.sign-up').click(function() {
