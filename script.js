4bd7af363d8f855142eeadd47f71f639ea3e32e6
dirgepye <s.stenhouse87@gmail.com>
2016-04-25 05:07:12 +0000

    main page created, title animation

--- script.js
+++ script.js
@@ -1,90 +1,108 @@
-$(document).ready(function() {
-	// $('.style').click(function() {
-	// 	console.log('test');
-	// 	//var e = document.getElementsByClassName('form-basic');
+console.log("wow!")
+
+var shawn = document.getElementById("shawn"),
+// 	sten = document.getElementById("sten"),
+// 	house = document.getElementById("house"),
+	
+// console.log(sten);
+
+// TweenLite.to(shawn, 1, {opacity: 0});
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+// 	// $('.style').click(function() {
+// 	// 	console.log('test');
+// 	// 	//var e = document.getElementsByClassName('form-basic');
 		
-	// 	$('.form-basic').toggleClass('style-class');
-	// 	//$('.form-basic').removeClass('style-class-two')
+// 	// 	$('.form-basic').toggleClass('style-class');
+// 	// 	//$('.form-basic').removeClass('style-class-two')
 		
-	// });
-	// $('.ringading').click(function() {
-	// 	console.log('test');
-	// 	//var e = document.getElementsByClassName('form-basic');
-	// 	$('.form-basic').toggleClass('style-class-two');
-	// });
-	// $('.doodledoo').click(function() {
-	// 	console.log('test');
-	// 	//var e = document.getElementsByClassName('form-basic');
-	// 	$('.form-basic').toggleClass('style-class-three');
-	// });
+// 	// });
+// 	// $('.ringading').click(function() {
+// 	// 	console.log('test');
+// 	// 	//var e = document.getElementsByClassName('form-basic');
+// 	// 	$('.form-basic').toggleClass('style-class-two');
+// 	// });
+// 	// $('.doodledoo').click(function() {
+// 	// 	console.log('test');
+// 	// 	//var e = document.getElementsByClassName('form-basic');
+// 	// 	$('.form-basic').toggleClass('style-class-three');
+// 	// });
 	
 	
-	$('.style-one').on('click', function() {
-		if ($('.form-a__basic').hasClass('style-class-two style-class-three')) {
-			$('.form-a__basic').removeClass('style-class-two style-class-three');
-			$('.form-a__basic').addClass('style-class-one');
-		}
+// 	$('.style-one').on('click', function() {
+// 		if ($('.form-a__basic').hasClass('style-class-two style-class-three')) {
+// 			$('.form-a__basic').removeClass('style-class-two style-class-three');
+// 			$('.form-a__basic').addClass('style-class-one');
+// 		}
 		
-		else if ($('.form-a__basic').hasClass('style-class-one')) {
-			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
-		}
-		else {
-			$('.form-a__basic').addClass('style-class-one');
-		}
-	});
+// 		else if ($('.form-a__basic').hasClass('style-class-one')) {
+// 			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
+// 		}
+// 		else {
+// 			$('.form-a__basic').addClass('style-class-one');
+// 		}
+// 	});
 	
-	$('.style-two').on('click', function() {
-		if ($('.form-a__basic').hasClass('style-class-one style-class-three')) {
-			$('.form-a__basic').removeClass('style-class-one style-class-three');
-			$('.form-a__basic').addClass('style-class-two');
-		}
+// 	$('.style-two').on('click', function() {
+// 		if ($('.form-a__basic').hasClass('style-class-one style-class-three')) {
+// 			$('.form-a__basic').removeClass('style-class-one style-class-three');
+// 			$('.form-a__basic').addClass('style-class-two');
+// 		}
 		
-		else if ($('.form-a__basic').hasClass('style-class-two')) {
-			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
-		}
-		else {
-			$('.form-a__basic').addClass('style-class-two');
-		}
-	});
+// 		else if ($('.form-a__basic').hasClass('style-class-two')) {
+// 			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
+// 		}
+// 		else {
+// 			$('.form-a__basic').addClass('style-class-two');
+// 		}
+// 	});
 	
-	$('.style-three').on('click', function() {
-		if ($('.form-a__basic').hasClass('style-class-one style-class-two')) {
-			$('.form-a__basic').removeClass('style-class-one style-class-two');
-			$('.form-a__basic').addClass('style-class-three');
-		}
+// 	$('.style-three').on('click', function() {
+// 		if ($('.form-a__basic').hasClass('style-class-one style-class-two')) {
+// 			$('.form-a__basic').removeClass('style-class-one style-class-two');
+// 			$('.form-a__basic').addClass('style-class-three');
+// 		}
 		
-		else if ($('.form-a__basic').hasClass('style-class-three')) {
-			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
-		}
-		else {
-			$('.form-a__basic').addClass('style-class-three');
-		}
-	});
+// 		else if ($('.form-a__basic').hasClass('style-class-three')) {
+// 			$('.form-a__basic').removeClass('style-class-one style-class-two style-class-three');
+// 		}
+// 		else {
+// 			$('.form-a__basic').addClass('style-class-three');
+// 		}
+// 	});
 	
-	$('.form-four').parallax({imageSrc : 'forest_bg.jpg'});
+// 	$('.form-four').parallax({imageSrc : 'forest_bg.jpg'});
 	
-	$('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
-	
-	
-	
-	$('.sign-up').click(function() {
-		console.log('test');
-		$('.fields').append('<input type="text" id="login-username" placeholder="username" class="form-two__field"><input type="text" id="login-password" placeholder="password" class="form-two__field"><input type="text" id="login-username" placeholder="confirm password" class="form-two__field">');
-	});
+// 	$('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
 	
 	
 	
+// 	$('.sign-up').click(function() {
+// 		console.log('test');
+// 		$('.fields').append('<input type="text" id="login-username" placeholder="username" class="form-two__field"><input type="text" id="login-password" placeholder="password" class="form-two__field"><input type="text" id="login-username" placeholder="confirm password" class="form-two__field">');
+// 	});
 	
-	$(".navbar").hide();
-	$(function() {
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 100) {
-				$('.navbar').fadeIn();
-			}
-			else {
-				$('.navbar').fadeOut();
-			}
-		});
-	});
 	
-});
+	
+	
+// 	$(".navbar").hide();
+// 	$(function() {
+// 		$(window).scroll(function() {
+// 			if ($(this).scrollTop() > 100) {
+// 				$('.navbar').fadeIn();
+// 			}
+// 			else {
+// 				$('.navbar').fadeOut();
+// 			}
+// 		});
+// 	});
+	
+// });
